7d519909fa9c4bc5a2e61f0a1ffbc45a402df911
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2021-07-08 20:18:02 +0800

    Update substrate

--- src/command/permastore.rs
+++ src/command/permastore.rs
@@ -1,7 +1,9 @@
 use anyhow::Result;
-use sp_runtime::traits::{BlakeTwo256, Hash};
 use sp_core::Encode;
+use sp_runtime::traits::{BlakeTwo256, Hash};
 use structopt::StructOpt;
+
+use cp_permastore::CHUNK_SIZE;
 
 use crate::{
     runtime::{pallets::permastore::StoreCallExt, CanyonSigner},
@@ -24,13 +26,17 @@ impl Permastore {
 
         match self {
             Self::Store { data } => {
-                const CHUNK_SIZE: usize = 256 * 1024;
-
                 let data = data.as_bytes().to_vec();
-                let chunks = data.chunks(CHUNK_SIZE).map(|c| BlakeTwo256::hash(c).encode()).collect();
+                let chunks = data
+                    .chunks(CHUNK_SIZE as usize)
+                    .map(|c| BlakeTwo256::hash(c).encode())
+                    .collect();
+
                 let chunk_root = BlakeTwo256::ordered_trie_root(chunks);
                 println!("chunk root: {:?}", chunk_root);
+
                 let data_size = data.len() as u32;
+                println!("data size: {:?}", data_size);
 
                 let result = client.store(&signer, data_size, chunk_root, data).await?;
                 println!("Store result: {:?}", result);
