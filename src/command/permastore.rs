2fda8c9666ece144da4a278089684e221afb39c4
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2021-07-08 21:16:56 +0800

    .

--- src/command/permastore.rs
+++ src/command/permastore.rs
@@ -1,4 +1,6 @@
-use anyhow::Result;
+use std::path::PathBuf;
+
+use anyhow::{anyhow, Result};
 use sp_core::Encode;
 use sp_runtime::traits::{BlakeTwo256, Hash};
 use structopt::StructOpt;
@@ -6,7 +8,7 @@ use structopt::StructOpt;
 use cp_permastore::CHUNK_SIZE;
 
 use crate::{
-    runtime::{pallets::permastore::StoreCallExt, CanyonSigner},
+    runtime::{pallets::permastore::StoreCallExt, CanyonClient, CanyonSigner},
     utils::build_client,
 };
 
@@ -15,9 +17,29 @@ use crate::{
 pub enum Permastore {
     /// Store the data.
     Store {
-        #[structopt(index = 1, long)]
-        data: String,
+        #[structopt(long)]
+        data: Option<String>,
+        #[structopt(short, long, parse(from_os_str), conflicts_with = "data")]
+        path: Option<PathBuf>,
     },
+}
+
+async fn store(client: &CanyonClient, signer: &CanyonSigner, data: Vec<u8>) -> Result<()> {
+    let chunks = data
+        .chunks(CHUNK_SIZE as usize)
+        .map(|c| BlakeTwo256::hash(c).encode())
+        .collect();
+
+    let chunk_root = BlakeTwo256::ordered_trie_root(chunks);
+    println!("chunk root: {:?}", chunk_root);
+
+    let data_size = data.len() as u32;
+    println!("data size: {:?}", data_size);
+
+    let result = client.store(signer, data_size, chunk_root, data).await?;
+    println!("Store result: {:?}", result);
+
+    Ok(())
 }
 
 impl Permastore {
@@ -25,21 +47,16 @@ impl Permastore {
         let client = build_client(url).await?;
 
         match self {
-            Self::Store { data } => {
-                let data = data.as_bytes().to_vec();
-                let chunks = data
-                    .chunks(CHUNK_SIZE as usize)
-                    .map(|c| BlakeTwo256::hash(c).encode())
-                    .collect();
-
-                let chunk_root = BlakeTwo256::ordered_trie_root(chunks);
-                println!("chunk root: {:?}", chunk_root);
-
-                let data_size = data.len() as u32;
-                println!("data size: {:?}", data_size);
-
-                let result = client.store(&signer, data_size, chunk_root, data).await?;
-                println!("Store result: {:?}", result);
+            Self::Store { data, path } => {
+                if let Some(data) = data {
+                    let data = data.as_bytes().to_vec();
+                    store(&client, &signer, data).await?;
+                } else if let Some(path) = path {
+                    let data = std::fs::read(path)?;
+                    store(&client, &signer, data).await?;
+                } else {
+                    return Err(anyhow!("--data or --path is required for store command"));
+                }
             }
         }
 
